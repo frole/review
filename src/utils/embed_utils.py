014ef302571f80e11fda75a020f55073d6119063
BAR-Rei <guilhempiat@yahoo.fr>
2018-05-18 17:28:06 +0200

    Implemented create_doc_embeddings Documented code Created a couple of utility functions

--- src/utils/embed_utils.py
+++ src/utils/embed_utils.py
@@ -40,7 +40,15 @@ def create_word_embeddings(corpusname,
     return model
 
 
-def json_to_tagged_docs(json_doc):
+def _json_to_tagged_docs(json_doc):
+    """ Turns a document loaded from a corpus into 2 TaggedDocuments
+        Arguments:
+            - (dict) json_doc: a document read with `json.loads` from a corpus
+        Returns:
+            - (TaggedDocument): TaggedDocument for the abstract
+                (or None if there is none)
+            - (TaggedDocument): TaggedDocument for the body of the article
+    """
     import re
     import string
     from gensim.models.doc2vec import TaggedDocument
@@ -77,18 +85,64 @@ def json_to_tagged_docs(json_doc):
     return abstract_tagged_doc, TaggedDocument(fulltext, tags)
 
 
-def create_doc_embeddings(corpusnames):
-    from gensim.models.doc2vec import Doc2Vec
+def corpus_to_tagged_docs(corpusname):
+    """ Generator for TaggedDocuments from a corpus
+        Arguments:
+            - (str) corpusname : the name of the corpus to tag
+        Yields:
+            for each document in corpus:
+            - (TaggedDocument): TaggedDocument for the abstract
+                (if it is not None)
+            - (TaggedDocument): TaggedDocument for the body of the article
+    """
+    from misc_utils import get_docs_from_json_corpus
+    for doc in get_docs_from_json_corpus(corpusname=corpusname):
+        doc1, doc2 = _json_to_tagged_docs(doc)
+        if doc1 is not None:
+            yield doc1
+        yield doc2
 
-    Doc2Vec(documents=None,
-            dm_mean=None,
-            dm=1,
-            dbow_words=0,
-            dm_concat=0,
-            dm_tag_count=1,
-            docvecs=None,
-            docvecs_mapfile=None,
-            comment=None,
-            trim_rule=None,
-            callbacks=())
-    return Doc2Vec().wv
+
+def corpora_to_tagged_docs(corporanames):
+    """ Calls corpus_to_tagged_docs for multiple corpora
+    """
+    for corpusname in corporanames:
+        for tagged_doc in corpus_to_tagged_docs(corpusname):
+            yield tagged_doc
+
+
+def create_doc_embeddings(corporanames,
+                          dm=1,
+                          dbow_words=0,
+                          dm_concat=0,
+                          dm_tag_count=1,
+                          trim_rule=None):
+    """ Creates Document embeddings (paragraph vectors) for the specified corpora
+        Arguments:
+            - (list<str>) corporanames: a list of corpus names for which
+                embeddings should be created
+            - (int {1,0}) dm: Defines the training algorithm. If dm=1,
+                'distributed memory' is used. Otherwise, distributed
+                bag of words.
+            - (int {1,0}) dbow_words:
+                    1 - trains word-vectors (skip-gram)
+                    0 - only trains doc-vectors (faster)
+            - (int {1,0}) dm_concat:
+                    1 - use concatenation of context vectors
+                    0 - use sum/average of context vectors (smaller model)
+            - (int) dm_tag_count: Expected constant number of document tags
+                per document, when using dm_concat mode; default is 1.
+            - (function) trim_rule: Vocabulary trimming rule. Can be None
+                (min_count will be used), or a callable that accepts parameters
+                (word, count, min_count) and returns gensim.utils.RULE_DISCARD,
+                gensim.utils.RULE_KEEP or gensim.utils.RULE_DEFAULT (min_count
+                will be used). The rule is not stored as part of the model.
+    """
+    from gensim.models.doc2vec import Doc2Vec
+    model = Doc2Vec(documents=corpora_to_tagged_docs(corporanames),
+                    dm=dm,
+                    dbow_words=dbow_words,
+                    dm_concat=dm_concat,
+                    dm_tag_count=dm_tag_count,
+                    trim_rule=trim_rule)
+    return model
