52abd49fa8f67dc9378b92b80054017150995697
Syncrossus <syncrossus@gmail.com>
2018-07-02 10:19:21 +0200

    improved regular expressions

--- src/utils/regexes.py
+++ src/utils/regexes.py
@@ -4,18 +4,19 @@
 # Garbage matcher
 # matches any string consisting of only |, <, >, +, *, ^, #, =, and hyphen chains.
 # this is to identify patterns like ++<===> ######## <---->^^ which serve no purpose but to clutter the text
-PAT_GARBAGE = r"(\|*(--+)*<*>*\+*\**\^*#*=*)*"
+PAT_GARBAGE = r"(\||(--+)|<|>|\+|\*|\^|#|=)*"
 # layout hyphenation matcher, typically matches hyphens used as bullet points
 PAT_BULLET_HYPHENS = r"(\ -\ )"
 # e-mail address matcher
-PAT_EMAIL = r"([a-z]*[0-9]*\.*\+*)+@([a-z]*[0-9]*\.*-*)+\.[a-z]+"
+# the theoretical character limit for top-level domains is 63 characters
+PAT_EMAIL = r"([a-z]|[0-9]|\.|\+)+@([a-z]|[0-9]|\.|-)+\.[a-z]{2,63}"
 # phone number matcher
 PAT_PHONE = r"\+?([0-9](-|\ )?){9, 11}"
 # URL matcher
-PAT_URL = r"https?\://(www\.)?([a-z]*[0-9]*\.?-?%?)+\.[a-z]+(/([a-z]*[0-9]*-*.+)*)*/?(\?([a-z]*[0-9]*\.?-?%?=?&?_?#?\:?)+)?"
+PAT_URL = r"https?\://(www\.)?([a-z]|[0-9]|\.|-|%)+\.[a-z]{2,63}(/([a-z]|[0-9]|-|\.|_|#)+)*/?(\?([a-z]|[0-9]|\.|-|%|=|&|_|#|\:|\+)+)?"
 
-# https?\://(www\.)?
-# ([a-z]*[0-9]*\.?-?%?)+ 			# adress
-# \.[a-z]+							# domain name
-# (/([a-z]*[0-9]*-*.+)*)*/?			# /stuff/between/slashes/
-# (\?([a-z]*[0-9]*\.?-?%?=?)+)?		# request details after ?
+# https?\://(www\.)?                            # pretty self explanatory
+# ([a-z]|[0-9]|\.|-|%)+                         # adress
+# \.[a-z]{2,63}                                 # domain name
+# (/([a-z]|[0-9]|-|\.|_|#)+)*/?                 # /stuff/between/slashes/
+# (\?([a-z]|[0-9]|\.|-|%|=|&|_|#|\:|\+)+)?"     # request details after ?
