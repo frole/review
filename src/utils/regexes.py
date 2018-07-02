325882d84c22e988fe52b7a90ab48d48f081df49
Syncrossus <syncrossus@gmail.com>
2018-07-02 09:42:43 +0200

    Changed systematically escaped str to raw str for readability

--- src/utils/regexes.py
+++ src/utils/regexes.py
@@ -4,15 +4,15 @@
 # Garbage matcher
 # matches any string consisting of only |, <, >, +, *, ^, #, =, and hyphen chains.
 # this is to identify patterns like ++<===> ######## <---->^^ which serve no purpose but to clutter the text
-PAT_GARBAGE = "(\\|*(--+)*<*>*\\+*\\**\\^*#*=*)*"
+PAT_GARBAGE = r"(\|*(--+)*<*>*\+*\**\^*#*=*)*"
 # layout hyphenation matcher, typically matches hyphens used as bullet points
-PAT_BULLET_HYPHENS = "(\\ -\\ )"
+PAT_BULLET_HYPHENS = r"(\ -\ )"
 # e-mail address matcher
-PAT_EMAIL = "([a-z]*[0-9]*\\.*\\+*)+@([a-z]*[0-9]*\\.*-*)+\\.[a-z]+"
+PAT_EMAIL = r"([a-z]*[0-9]*\.*\+*)+@([a-z]*[0-9]*\.*-*)+\.[a-z]+"
 # phone number matcher
-PAT_PHONE = "\\+?([0-9](-|\\ )?){9, 11}"
+PAT_PHONE = r"\+?([0-9](-|\ )?){9, 11}"
 # URL matcher
-PAT_URL = "https?\\://(www\\.)?([a-z]*[0-9]*\\.?-?%?)+\\.[a-z]+(/([a-z]*[0-9]*-*.+)*)*/?(\\?([a-z]*[0-9]*\\.?-?%?=?&?_?#?\\:?)+)?"
+PAT_URL = r"https?\://(www\.)?([a-z]*[0-9]*\.?-?%?)+\.[a-z]+(/([a-z]*[0-9]*-*.+)*)*/?(\?([a-z]*[0-9]*\.?-?%?=?&?_?#?\:?)+)?"
 
 # https?\://(www\.)?
 # ([a-z]*[0-9]*\.?-?%?)+ 			# adress
