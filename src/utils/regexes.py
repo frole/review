29cbf98515a0e8a9af012df2dff57e18cca44cae
Syncrossus <syncrossus@gmail.com>
2018-07-03 14:17:44 +0200

    improved regular expressions

--- src/utils/regexes.py
+++ src/utils/regexes.py
@@ -6,12 +6,12 @@
 # this is to identify patterns like ++<===> ######## <---->^^ which serve no purpose but to clutter the text
 PAT_GARBAGE = r"((\||(--+)|(__+)|<|>|\+|\*|\^|#|=|~)+|(\\|_|/){2,})"
 # layout hyphenation matcher, typically matches hyphens used as bullet points
-PAT_BULLET_HYPHENS = r"(\ -\ )"
+PAT_BULLET_HYPHENS = r"(^\ *-\ )"
 # e-mail address matcher
 # the theoretical character limit for top-level domains is 63 characters
 PAT_EMAIL = r"(([A-z]|[0-9]|\.|\+)+@([A-z]|[0-9]|\.|-)+\.[A-z]{2,63})"
 # phone number matcher
-PAT_PHONE = r"(\+?([0-9](-|\ )?){8,10}[0-9])"
+PAT_PHONE = r"(\(?\+?(\(?[0-9]\)?(-|\ |\.)?){6,30}[0-9])"
 # URL matcher
 PAT_URL = r"(https?\://(www\.)?([A-z]|[0-9]|\.|-|%)+\.[A-z]{2,63}(/([A-z]|[0-9]|-|\.|_|#)+)*/?(\?([A-z]|[0-9]|\.|-|%|=|&|_|#|\:|\+)+)?)"
 
